5de812cb7c34226712981d41a4c97d8beb2451ed
culled <coastersandthrills@gmail.com>
2023-03-03 14:34:14 -0800

    Implemented random class

--- src/Coco/Core/Math/Random.cpp
+++ src/Coco/Core/Math/Random.cpp
@@ -1,6 +1,54 @@
 #include "Random.h"
+#include <Coco/Core/Engine.h>
+#include <Coco/Core/Platform/IEnginePlatform.h>
 
 namespace Coco
 {
+    bool Random::_isSeeded = false;
+    unsigned int Random::_seed = 0;
 
+    int Random::RandomRange(int min, int max)
+    {
+        EnsureSeeded();
+
+        std::uniform_int_distribution<int> distribution(min, max);
+
+        return distribution(_generator);
+    }
+
+    double Random::RandomRange(double min, double max)
+    {
+        EnsureSeeded();
+
+        std::uniform_real_distribution<double> distribution(min, max);
+
+        return distribution(_generator);
+    }
+
+    double Random::RandomValue()
+    {
+        return RandomRange(0.0, 1.0);
+    }
+
+    void Random::SetSeed(unsigned int seed)
+    {
+        _seed = seed;
+        _generator = std::default_random_engine(seed);
+        _isSeeded = true;
+    }
+
+    unsigned int Random::GetSeed()
+    {
+        EnsureSeeded();
+
+        return _seed;
+    }
+
+    void Random::EnsureSeeded()
+    {
+        if (_isSeeded)
+            return;
+
+        SetSeed(static_cast<unsigned int>(Engine::Get()->GetPlatform()->GetPlatformTimeSeconds()));
+    }
 }
--- src/Coco/Core/Math/Random.h
+++ src/Coco/Core/Math/Random.h
@@ -1,15 +1,59 @@
 #pragma once
 
 #include <Coco/Core/Core.h>
+#include <random>
 
 namespace Coco
 {
 	class COCOAPI Random
 	{
+	private:
+		static bool _isSeeded;
+		static unsigned int _seed;
+
+		static std::default_random_engine _generator;
+
 	public:
+		/// <summary>
+		/// Generates a psuedorandom value in the range [min, max].
+		/// Minimum and maximum are inclusive
+		/// </summary>
+		/// <param name="min">The minimum bound (inclusive)</param>
+		/// <param name="max">The maximum bound (inclusive)</param>
+		/// <returns>A psuedorandom integer between min and max</returns>
 		static int RandomRange(int min, int max);
+
+		/// <summary>
+		/// Generates a psuedorandom value in the range [min, max).
+		/// Minimum is inclusive, maximum is exclusive
+		/// </summary>
+		/// <param name="min">The minimum bound (inclusive)</param>
+		/// <param name="max">The maximum bound (exclusive)</param>
+		/// <returns>A psuedorandom dobule between min and max</returns>
 		static double RandomRange(double min, double max);
 
+		/// <summary>
+		/// Generates a psuedorandom value in the range [0.0, 1.0)
+		/// </summary>
+		/// <returns>A psuedorandom double between 0.0 and 1.0</returns>
 		static double RandomValue();
+
+		/// <summary>
+		/// Sets the seed of the psuedorandom number generator
+		/// </summary>
+		/// <param name="seed">The seed for the psuedorandom number generator</param>
+		static void SetSeed(unsigned int seed);
+
+		/// <summary>
+		/// Gets the current seed used by the psuedorandom number generator
+		/// </summary>
+		/// <returns>The current seed</returns>
+		static unsigned int GetSeed();
+
+	private:
+		/// <summary>
+		/// Ensures the psuedorandom number generator has been seeded, either manually or by the clock
+		/// </summary>
+		static void EnsureSeeded();
 	};
 }
