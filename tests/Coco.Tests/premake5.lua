ea6f0a769f9b280ec57fd3464d5913724700c7b4
culled <coastersandthrills@gmail.com>
2023-08-23 22:35:37 -0700

    Fixed testing premake

--- tests/Coco.Tests/premake5.lua
+++ tests/Coco.Tests/premake5.lua
@@ -2,6 +2,7 @@ project "Coco.Tests"
     kind "SharedLib"
     language "C++"
     cppdialect "C++20"
+    staticruntime "on"
 
     defines {"COCO_ASSERTIONS"}
     
@@ -26,3 +27,18 @@ project "Coco.Tests"
     {
         "Coco",
     }
+
+    filter { "configurations:Debug" }
+        defines
+        { 
+            "COCO_LOG_TRACE",
+            "COCO_LOG_INFO",
+            "COCO_LOG_WARNING",
+        }
+
+        runtime "Debug"
+        symbols "on"
+        
+    filter { "configurations:Release" }
+        runtime "Release"
+        optimize "on"
